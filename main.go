24f1a54663565d59ef24b8362c120d3b897af2b9
Vitor Basso <vitorbasso93@gmail.com>
2024-03-21 22:29:30 -0300

    final tweaks

--- main.go
+++ main.go
@@ -10,22 +10,22 @@ import (
 )
 
 var (
-	cep = flag.String("cep", "01153000", "CEP to search")
+	cep = *flag.String("cep", "01153000", "CEP to search")
 )
 
 func init() {
 	flag.Parse()
-	if *cep == "" {
-		log.Fatal("CEP is required")
+	if cep == "" {
+		log.Panic("CEP is required")
 	}
 }
 
 func main() {
-	viaCepChan := make(chan *viacepResponse)
-	brasilApiChan := make(chan *brasilapiResponse)
+	viaCepChan := make(chan viacepResponse)
+	brasilApiChan := make(chan brasilapiResponse)
 
-	go getViaCep(viaCepChan, *cep)
-	go getBrasilApi(brasilApiChan, *cep)
+	go getViaCep(viaCepChan, cep)
+	go getBrasilApi(brasilApiChan, cep)
 
 	select {
 	case viaCep := <-viaCepChan:
@@ -33,14 +33,14 @@ func main() {
 	case brasilApi := <-brasilApiChan:
 		print(brasilApi, "brasilApi")
 	case <-time.After(1 * time.Second):
-		log.Fatalf("Timeout")
+		log.Println("Timeout")
 	}
 }
 
 func print(response any, from string) {
 	res, err := json.Marshal(response)
 	if err != nil {
-		log.Fatalf(fmt.Errorf("print error: %w", err).Error())
+		log.Panic(fmt.Errorf("print error: %w", err).Error())
 	}
 	log.Printf("Response through %s:\n %s", from, string(res))
 }
@@ -58,19 +58,24 @@ type viacepResponse struct {
 	Siafi       string `json:"siafi"`
 }
 
-func getViaCep(ch chan<- *viacepResponse, cep string) {
-	res, err := http.Get("https://viacep.com.br/ws/" + cep + "/json")
+func getViaCep(ch chan<- viacepResponse, cep string) {
+	client := &http.Client{Timeout: 1001 * time.Millisecond}
+	res, err := client.Get("https://viacep.com.br/ws/" + cep + "/json")
 	if err != nil {
-		log.Println(fmt.Errorf("getViaCep error: %w", err))
+		log.Printf("getViaCep error: %+v", err)
 		return
 	}
 	defer res.Body.Close()
-	var address viacepResponse
-	if err := json.NewDecoder(res.Body).Decode(&address); err != nil {
-		log.Println(fmt.Errorf("getViaCep error: %w", err))
+	if res.StatusCode != http.StatusOK {
+		log.Printf("getViaCep error: %+v", fmt.Errorf("status code is not 200"))
 		return
 	}
-	ch <- &address
+	var address viacepResponse
+	if err := json.NewDecoder(res.Body).Decode(&address); err != nil {
+		log.Printf("getViaCep error: %+v", err)
+		return
+	}
+	ch <- address
 }
 
 type brasilapiResponse struct {
@@ -82,17 +87,22 @@ type brasilapiResponse struct {
 	Service      string `json:"service"`
 }
 
-func getBrasilApi(ch chan<- *brasilapiResponse, cep string) {
-	res, err := http.Get("https://brasilapi.com.br/api/cep/v1/" + cep)
+func getBrasilApi(ch chan<- brasilapiResponse, cep string) {
+	client := &http.Client{Timeout: 1001 * time.Millisecond}
+	res, err := client.Get("https://brasilapi.com.br/api/cep/v1/" + cep)
 	if err != nil {
-		log.Println(fmt.Errorf("getBrasilApi error: %w", err))
+		log.Printf("getBrasilApi error: %+v", err)
 		return
 	}
 	defer res.Body.Close()
-	var address brasilapiResponse
-	if err := json.NewDecoder(res.Body).Decode(&address); err != nil {
-		log.Println(fmt.Errorf("getBrasilApi error: %w", err))
+	if res.StatusCode != http.StatusOK {
+		log.Printf("getBrasilApi error: %+v", fmt.Errorf("status code is not 200"))
 		return
 	}
-	ch <- &address
+	var address brasilapiResponse
+	if err := json.NewDecoder(res.Body).Decode(&address); err != nil {
+		log.Printf("getBrasilApi error: %+v", err)
+		return
+	}
+	ch <- address
 }
